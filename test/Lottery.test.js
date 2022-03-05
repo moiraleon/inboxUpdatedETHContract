977dfbda18213663c2a7ff25166602a49909e6b8
Moira Leon <moira.leon2@gmail.com>
2022-03-05 14:52:49 -0700

    testing funciton modifier of pickwinner function

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -55,17 +55,27 @@ describe ('Lottery Contract', ()=>{
         assert.equal(accounts[2], players[2]);
         assert.equal(3, players.length);
     });
-    it('requires a minimum amount of ether to enter', async () => {
-        try { 
-        await lottery.methods.enter().send({
-            from: accounts[0],
-            value: 0
-        });
-        assert(false);
-        } catch (err) {
-            assert(err);
-        }
+        it('requires a minimum amount of ether to enter', async () => {
+            try { 
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: 0
+            });
+            assert(false);
+            } catch (err) {
+                assert(err);
+            }
     });
-        
+
+    it('only manager can call pickWinner', async () => {
+        try {
+        await lottery.methods.pickWinner().send({
+        from: accounts[1]
+        });
+         assert(false);
+         } catch (err) {
+        assert(err);
+         }
+    });   
 
 });
